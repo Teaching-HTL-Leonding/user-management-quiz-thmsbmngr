423eeea3adc0d320f458e9e7a340d942d341c976
Thomas Baminger <thms.baminger@gmail.com>
2021-03-25 11:40:49 +0100

    Add subgroups to a group

--- Start/Data/Group.cs
+++ Start/Data/Group.cs
@@ -11,5 +11,9 @@ namespace UserManagement.Data
         public string Name { get; set; } = string.Empty;
 
         public List<User>? Users { get; set; } = new List<User>();
+
+        public Group? ParentGroup { get; set; }
+        
+        public List<Group>? ChildGroups { get; set; }
     }
 }
---
+++ Start/Migrations/20210325103354_AddSubgroups.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.Migrations
+{
+    public partial class AddSubgroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParentGroupId",
+                table: "Groups",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_ParentGroupId",
+                table: "Groups",
+                column: "ParentGroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Groups_ParentGroupId",
+                table: "Groups",
+                column: "ParentGroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Groups_ParentGroupId",
+                table: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_ParentGroupId",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "ParentGroupId",
+                table: "Groups");
+        }
+    }
+}
